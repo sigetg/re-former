706ba1287f726961ab92ae0330a2622e132e3a90
George Sigety <89882158+sigetg@users.noreply.github.com>
2023-07-27 18:15:30 -0400

    Add edit user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,20 @@ class UsersController < ApplicationController
   end
 
   def show
-    
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   private
